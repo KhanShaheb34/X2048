407109db55196b3aee2ead6643fca101d5345a8f
KhanShaheb34 <shakirulhkhan@gmail.com>
2024-09-28 21:15:10 +0600

    feat: movement logic

--- X2048/GameLogic.swift
+++ X2048/GameLogic.swift
@@ -23,4 +23,110 @@ struct GameLogic {
             grid[postion.0][postion.1] = [2, 4].randomElement()!
         }
     }
+
+    static func moveLeft(_ grid: [[Int]]) -> [[Int]] {
+        var newGrid = grid
+
+        for i in 0..<4 {
+            var row = newGrid[i].filter { $0 != 0 }
+            if row.count > 0 {
+                for j in 0..<(row.count - 1) {
+                    if row[j] == row[j + 1] {
+                        row[j] *= 2
+                        row[j + 1] = 0
+                    }
+                }
+            }
+            row = row.filter { $0 != 0 }
+            while row.count < 4 {
+                row.append(0)
+            }
+            newGrid[i] = row
+        }
+
+        addRandomTile(to: &newGrid)
+        return newGrid
+    }
+
+
+    static func moveRight(_ grid: [[Int]]) -> [[Int]] {
+        var newGrid = grid
+        
+        for i in 0..<4 {
+            var row = newGrid[i].reversed().filter { $0 != 0 }
+            if row.count > 0 {
+                for j in 0..<(row.count - 1) {
+                    if row[j] == row[j + 1] {
+                        row[j] *= 2
+                        row[j + 1] = 0
+                    }
+                }
+            }
+            row = row.filter { $0 != 0 }
+            while row.count < 4 {
+                row.append(0)
+            }
+            newGrid[i] = row.reversed()
+        }
+        
+        addRandomTile(to: &newGrid)
+        return newGrid
+    }
+
+    static func moveUp(_ grid: [[Int]]) -> [[Int]] {
+        var newGrid = grid
+        
+        for j in 0..<4 {
+            var col = newGrid.map(\.self[j]).filter{ $0 != 0 }
+            if(col.count > 0) {
+                for i in 0..<(col.count - 1) {
+                    if col[i] == col[i + 1] {
+                        col[i] *= 2
+                        col[i + 1] = 0
+                    }
+                }
+            }
+            col = col.filter { $0 != 0 }
+            while col.count < 4 {
+                col.append(0)
+            }
+            
+            for(i, value) in col.enumerated() {
+                newGrid[i][j] = value
+            }
+        }
+        
+        addRandomTile(to: &newGrid)
+        return newGrid
+    }
+
+    static func moveDown(_ grid: [[Int]]) -> [[Int]] {
+        var newGrid = grid
+        
+        for j in 0..<4 {
+            var col = newGrid.map(\.self[j]).reversed().filter{ $0 != 0 }
+            if(col.count > 0) {
+                for i in 0..<(col.count - 1) {
+                    if col[i] == col[i + 1] {
+                        col[i] *= 2
+                        col[i + 1] = 0
+                    }
+                }
+            }
+            
+            col = col.filter { $0 != 0 }
+            while col.count < 4 {
+                col.append(0)
+            }
+            
+            for(i, value) in col.reversed().enumerated() {
+                newGrid[i][j] = value
+            }
+        }
+        
+        addRandomTile(to: &newGrid)
+        return newGrid
+    }
+    
+
 }
--- X2048/GameView.swift
+++ X2048/GameView.swift
@@ -14,6 +14,28 @@ struct GameView: View {
                 .padding()
             GridView(grid: $grid)
         }
+        .gesture(
+            DragGesture()
+                .onEnded { value in
+                    let horizontalOffset = value.translation.width
+                    let verticalOffset = value.translation.height
+                    
+                    if abs(horizontalOffset) > abs(verticalOffset) {
+                        if(horizontalOffset < 0) {
+                            self.grid = GameLogic.moveLeft(self.grid)
+                        } else {
+                            self.grid = GameLogic.moveRight(self.grid)
+                        }
+                    } else {
+                        if(verticalOffset < 0) {
+                            self.grid = GameLogic.moveUp(self.grid)
+                        } else {
+                            self.grid = GameLogic.moveDown(self.grid)
+                        }
+                    }
+                    
+                }
+        )
     }
 }
 
