977ce1900a5871b690db88fa548d43a7eafd37e5
KhanShaheb34 <shakirulhkhan@gmail.com>
2024-09-28 20:10:39 +0600

    Initial Commit

---
+++ X2048/X2048App.swift
@@ -0,0 +1,17 @@
+//
+//  X2048App.swift
+//  X2048
+//
+//  Created by Shakirul Hasan Khan on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct X2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X2048Tests/X2048Tests.swift
@@ -0,0 +1,17 @@
+//
+//  X2048Tests.swift
+//  X2048Tests
+//
+//  Created by Shakirul Hasan Khan on 9/28/24.
+//
+
+import Testing
+@testable import X2048
+
+struct X2048Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
